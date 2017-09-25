22a8f88e3c745a10498104105c478f78f0592997
Xiaoyu Zhang <xiaoyu.zhang@xzhang-mbp.corp.symphony.com>
2017-09-25 13:14:01 -0700

    add color theme

--- app.js
+++ app.js
@@ -1,16 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import classNames from 'classnames';
 
 import ImageLoader from './ImageLoader';
+import './style.scss';
 
-const ComponentList = () => {
-    return (
-        <div>
-            <ImageLoader dimension={200} />
-            <ImageLoader dimension={300} />
-            <ImageLoader dimension={400} theme={'dark'} />
-        </div>
-    );
+const getClassNames = (state) => {
+    const {
+        darkTheme,
+        contrast
+    } = state;
+
+    return classNames({
+        dark: darkTheme,
+        light: !darkTheme,
+        contrast,
+    });
+}
+
+class ComponentList extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            darkTheme: false,
+            contrast: false,
+        };
+        this.onToggleTheme = this.onToggleTheme.bind(this);
+        this.onToggleContrast = this.onToggleContrast.bind(this);
+    }
+
+    onToggleTheme()  {
+        this.setState({
+            darkTheme: !this.state.darkTheme
+        });
+    }
+
+    onToggleContrast() {
+        this.setState({
+            contrast: !this.state.contrast
+        });
+    }
+
+    render() {
+        return (
+            <div className={getClassNames(this.state)}>
+                <div className="controls">
+                    <button onClick={this.onToggleTheme}>
+                        Toggle theme
+                    </button>
+                    <label>
+                        Contrast
+                        <input type="checkbox" checked={this.state.contrast} onChange={this.onToggleContrast} />
+                    </label>
+                </div>
+                <ImageLoader dimension={200} />
+                <ImageLoader dimension={300} />
+                <ImageLoader dimension={400} theme={'dark'} />
+            </div>
+        );   
+    }
 };
 
 const div = document.createElement('div');
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,7 @@ const config = {
             }
         ]
     },
+    devtool: 'source-map',
     plugins: [
         new HtmlWebpackPlugin()
     ],
